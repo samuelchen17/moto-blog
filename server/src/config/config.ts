0a3985994a24b33a0ff04ee7cd0036135161900d
Samuel Chen <sche842@gmail.com>
2024-07-22 20:46:04 +1000

    project setup

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,6 +1,7 @@
 const config = {
   mongo: {
     options: {},
+    url: "mongodb+srv://admin:<password>@learning.jltuol3.mongodb.net/?retryWrites=true&w=majority&appName=Learning",
   },
   server: {
     host: "localhost",
