c50bcc6e9b5b450c50cc99a445061706f519a86f
Samuel Chen <sche842@gmail.com>
2024-07-26 21:29:26 +1000

    tailwind

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import IPageProps from "../interfaces/page";
 
 const HomePage = (props: IPageProps) => {
-  return <div>home page</div>;
+  return <div className="text-red-500">home page</div>;
 };
 
 export default HomePage;
