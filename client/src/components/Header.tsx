9923f2c5ce69f63d04286947e564cb4049b09373
Samuel Chen <sche842@gmail.com>
2024-07-27 17:30:36 +1000

    navbar prototype

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,23 +2,28 @@ import React from "react";
 import {
   Button,
   Navbar,
+  NavbarBrand,
   NavbarCollapse,
   NavbarLink,
   NavbarToggle,
   TextInput,
 } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import helmetIcon from "../assets/helmet.svg";
 import { IoMdSearch } from "react-icons/io";
 import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
+  const path = useLocation().pathname;
+
   return (
     <Navbar className="border-b-2">
-      <Link className="flex justify-center items-center gap-2" to="/">
-        <img src={helmetIcon} className="h-[60px]" />
-        <span className="text-4xl font-semibold">Sam's Moto Blog</span>
-      </Link>
+      <NavbarBrand as={"div"}>
+        <Link className="flex justify-center items-center gap-2" to="/">
+          <img src={helmetIcon} className="h-[60px]" />
+          <span className="text-4xl font-semibold">Sam's Moto Blog</span>
+        </Link>
+      </NavbarBrand>
       <form>
         <TextInput
           className="hidden lg:inline"
@@ -30,24 +35,25 @@ const Header = () => {
       <Button className="lg:hidden">
         <IoMdSearch />
       </Button>
-      <div className="flex gap-2">
+      {/* order-2 to place it at the end */}
+      <div className="flex gap-2 md:order-2">
         <Button className="hidden sm:inline" pill color="blue">
           <FaMoon />
         </Button>
-        <Link className="flex justify-center items-center gap-2" to="/login">
+        <Link to="/login">
           <Button gradientDuoTone="greenToBlue">Log In</Button>
         </Link>
         <NavbarToggle />
       </div>
 
       <NavbarCollapse>
-        <Navbar.Link active>
+        <NavbarLink active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
-        </Navbar.Link>
-        <NavbarLink active>
+        </NavbarLink>
+        <NavbarLink active={path === "/register"} as={"div"}>
           <Link to="/register">About</Link>
         </NavbarLink>
-        <NavbarLink active>
+        <NavbarLink active={path === "/edit"} as={"div"}>
           <Link to="/edit">Reviews</Link>
         </NavbarLink>
       </NavbarCollapse>
