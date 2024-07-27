d97744c9922eb93638c69b49b7d928ab4462f807
Samuel Chen <sche842@gmail.com>
2024-07-27 13:33:20 +1000

    layout

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,17 @@
+import { Navbar } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
+import helmetIcon from "../assets/helmet.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar className="border-b-2">
+      <Link className="flex justify-center items-center gap-2" to="/">
+        <img src={helmetIcon} className="h-[60px]" />
+        <span className="text-4xl font-semibold">Sam's Moto Blog</span>
+      </Link>
+    </Navbar>
+  );
 };
 
 export default Header;
--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -6,72 +6,39 @@ import ErrorPage from "../pages/ErrorPage";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import AuthWrapper from "../components/AuthWrapper";
+import Layout from "../components/Layout";
 
 const router = createBrowserRouter([
   {
-    path: "/login",
-    element: <AuthWrapper element={<LoginPage />} auth={false} />,
-  },
-  {
-    path: "/register",
-    element: <AuthWrapper element={<LoginPage />} auth={false} />,
-  },
-  {
-    path: "/edit",
-    element: <AuthWrapper element={<EditPage />} auth={true} />,
-  },
-  {
-    path: "/edit/:blogID",
-    element: <AuthWrapper element={<EditPage />} auth={true} />,
-  },
-  {
-    path: "/blogs/:blogID",
-    element: <AuthWrapper element={<BlogPage />} auth={false} />,
-  },
-  {
-    path: "/",
-    element: <AuthWrapper element={<HomePage />} auth={false} />,
+    element: <Layout />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/login",
+        element: <AuthWrapper element={<LoginPage />} auth={false} />,
+      },
+      {
+        path: "/register",
+        element: <AuthWrapper element={<LoginPage />} auth={false} />,
+      },
+      {
+        path: "/edit",
+        element: <AuthWrapper element={<EditPage />} auth={true} />,
+      },
+      {
+        path: "/edit/:blogID",
+        element: <AuthWrapper element={<EditPage />} auth={true} />,
+      },
+      {
+        path: "/blogs/:blogID",
+        element: <AuthWrapper element={<BlogPage />} auth={false} />,
+      },
+      {
+        path: "/",
+        element: <AuthWrapper element={<HomePage />} auth={false} />,
+      },
+    ],
   },
 ]);
 
 export default router;
-
-// const routes: IRoute[] = [
-//   {
-//     name: "Login",
-//     path: "/login",
-//     element: <LoginPage />,
-//     auth: false,
-//   },
-//   {
-//     name: "Sign Up",
-//     path: "/register",
-//     element: <LoginPage />,
-//     auth: false,
-//   },
-//   {
-//     name: "Create",
-//     path: "/edit",
-//     element: <EditPage />,
-//     auth: true,
-//   },
-//   {
-//     name: "Edit",
-//     path: "/edit/:blogID",
-//     element: <EditPage />,
-//     auth: true,
-//   },
-//   {
-//     name: "Blog",
-//     path: "/blogs/:blogID",
-//     element: <BlogPage />,
-//     auth: false,
-//   },
-//   {
-//     name: "Home",
-//     path: "/",
-//     element: <HomePage/>,
-//     auth: false,
-//   },
-// ];
