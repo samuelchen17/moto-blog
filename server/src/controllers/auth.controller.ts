dd396c5e988e60d9b1bb7234568aadfe8af89c86
Samuel Chen <sche842@gmail.com>
2024-07-29 17:49:46 +1000

    auth controller test

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,3 +1,26 @@
 import { Request, Response } from "express";
+import User from "../models/user.model";
 
-export const signup = async (req: Request, res: Response) => {};
+export const signup = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const newUser = new User({
+    // if name is similar, can just use else, username: username
+    username,
+    email,
+    password,
+  });
+
+  await newUser.save();
+};
