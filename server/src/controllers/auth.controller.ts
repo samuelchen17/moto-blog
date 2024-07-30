040c6f3c399ffdeee30d616b366fd0c2238e9183
Samuel Chen <sche842@gmail.com>
2024-07-31 00:28:43 +1000

    mongoose dup fix

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -4,16 +4,16 @@ import User from "../models/user.model";
 export const signup = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
 
-  if (
-    !username ||
-    !email ||
-    !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
-  ) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
+  //   if (
+  //     !username ||
+  //     !email ||
+  //     !password ||
+  //     username === "" ||
+  //     email === "" ||
+  //     password === ""
+  //   ) {
+  //     return res.status(400).json({ message: "All fields are required" });
+  //   }
 
   const newUser = new User({
     // if name is similar, can just use else, username: username
--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,18 +1,16 @@
 import mongoose, { Model, Schema } from "mongoose";
 import { IUser } from "../interface/user.interface";
 
-const userSchema: Schema<IUser> = new mongoose.Schema(
+const userSchema: Schema<IUser> = new mongoose.Schema<IUser>(
   {
     username: {
       type: String,
       required: [true, "Username is required"],
-      index: true,
       unique: true,
     },
     email: {
       type: String,
       required: true,
-      index: true,
       unique: true,
     },
     password: {
