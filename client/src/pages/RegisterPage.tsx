afc2c01a4a384ba46b19fbb9430f4a70694172ac
Samuel Chen <sche842@gmail.com>
2024-08-20 23:43:08 +1000

    register page UI

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import IPageProps from "../interfaces/page.interface";
+import { Button, Label, TextInput } from "flowbite-react";
 
 const RegisterPage = (props: IPageProps) => {
-  return <div className="min-h-screen">Register page</div>;
+  return (
+    <div className="min-h-screen">
+      <div></div>
+      <div>
+        <form>
+          <div>
+            <Label value="Your username" />
+            <TextInput type="text" placeholder="Username" id="username" />
+          </div>
+          <div>
+            <Label value="Your email" />
+            <TextInput type="text" placeholder="Email" id="email" />
+          </div>
+          <div>
+            <Label value="Your password" />
+            <TextInput type="text" placeholder="Password" id="password" />
+          </div>
+          <Button type="submit">Register</Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
