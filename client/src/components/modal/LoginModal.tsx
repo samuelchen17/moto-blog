da3c962ee5bdde7590b28799d2c9cfc1cd774b0f
Samuel Chen <sche842@gmail.com>
2024-08-21 23:04:52 +1000

    complete login modal

--- client/src/components/modal/LoginModal.tsx
+++ client/src/components/modal/LoginModal.tsx
@@ -8,54 +8,56 @@ interface LoginModalProps {
 const LoginModal: React.FC<LoginModalProps> = ({ emailInputRef }) => {
   return (
     <>
-      <Modal.Header />
+      <Modal.Header>Login</Modal.Header>
       <Modal.Body>
-        <div className="space-y-6">
-          <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-            Sign in to our platform
-          </h3>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="email" value="Your email" />
+        <form>
+          <div className="space-y-6">
+            <h3 className="text-xl font-medium text-gray-900 dark:text-white">
+              Sign in to our platform
+            </h3>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="email" value="Your email" />
+              </div>
+              <TextInput
+                id="email"
+                ref={emailInputRef}
+                placeholder="name@company.com"
+                required
+              />
             </div>
-            <TextInput
-              id="email"
-              ref={emailInputRef}
-              placeholder="name@company.com"
-              required
-            />
-          </div>
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="password" value="Your password" />
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="password" value="Your password" />
+              </div>
+              <TextInput id="password" type="password" required />
             </div>
-            <TextInput id="password" type="password" required />
-          </div>
-          <div className="flex justify-between">
-            <div className="flex items-center gap-2">
-              <Checkbox id="remember" />
-              <Label htmlFor="remember">Remember me</Label>
+            <div className="flex justify-between">
+              <div className="flex items-center gap-2">
+                <Checkbox id="remember" />
+                <Label htmlFor="remember">Remember me</Label>
+              </div>
+              <a
+                href="#"
+                className="text-sm text-cyan-700 hover:underline dark:text-cyan-500"
+              >
+                Lost Password?
+              </a>
             </div>
-            <a
-              href="#"
-              className="text-sm text-cyan-700 hover:underline dark:text-cyan-500"
-            >
-              Lost Password?
-            </a>
+            <div className="w-full">
+              <Button type="submit">Log in to your account</Button>
+            </div>
+            <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
+              Not registered?&nbsp;
+              <a
+                href="#"
+                className="text-cyan-700 hover:underline dark:text-cyan-500"
+              >
+                Create account
+              </a>
+            </div>
           </div>
-          <div className="w-full">
-            <Button>Log in to your account</Button>
-          </div>
-          <div className="flex justify-between text-sm font-medium text-gray-500 dark:text-gray-300">
-            Not registered?&nbsp;
-            <a
-              href="#"
-              className="text-cyan-700 hover:underline dark:text-cyan-500"
-            >
-              Create account
-            </a>
-          </div>
-        </div>
+        </form>
       </Modal.Body>
     </>
   );
